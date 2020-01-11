b40b77c203ecf855668f0fac2d3f6781d27b50b9
Ian Lancaster <ianclancaster@gmail.com>
2020-01-11 11:36:29 -0700

    Implement react-navigation.

--- src/app/App.component.tsx
+++ src/app/App.component.tsx
@@ -1,62 +1,17 @@
 import 'cross-fetch/polyfill'
 import React from 'react'
-import ApolloClient, { gql } from 'apollo-boost'
-import { ApolloProvider, useQuery } from '@apollo/react-hooks'
-
-import {
-  SafeAreaView,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native'
-
-import styles from './App.styles'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import Routes from 'src/routes'
 
 const client = new ApolloClient({
   uri: 'https://us1.prisma.sh/public-luckox-377/reservation-graphql-backend/dev',
 })
 
-const RESERVATIONS = gql`
-  {
-    reservations(orderBy: arrivalDate_ASC) {
-      id,
-      name,
-      hotelName,
-      arrivalDate,
-      departureDate
-    }
-  }
-`
-
-const Inner = () => {
-  const { loading, error, data } = useQuery(RESERVATIONS)
-  if (loading) return <Text>...loading</Text>
-  if (error) console.log(error.message)
-  console.log(data)
-  return (
-    <View>
-      {data.reservations.map((reservation: any) => (
-        <Text key={reservation.id}>{reservation.name}</Text>
-      ))}
-    </View>
-  )
-}
-
-const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <StatusBar barStyle='dark-content' />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior='automatic'
-          style={styles.scrollView}
-        >
-          <Inner />
-        </ScrollView>
-      </SafeAreaView>
-    </ApolloProvider>
-  )
-}
+const App = () => (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+)
 
 export default App
