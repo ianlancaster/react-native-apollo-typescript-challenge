2d40d26bfecf36b2de4d1ea51a0db97b5ccf63da
Ian Lancaster <ianclancaster@gmail.com>
2020-01-12 00:05:23 -0700

    Set up apollo:generate.

--- src/routes/reservations/Reservations.component.tsx
+++ src/routes/reservations/Reservations.component.tsx
@@ -3,9 +3,9 @@ import { useQuery } from '@apollo/react-hooks'
 import { Text, View } from 'react-native'
 import React from 'react'
 
-const RESERVATIONS = gql`
-  {
-    reservations(orderBy: arrivalDate_ASC) {
+const GET_RESERVATIONS = gql`
+  query GetReservationsData($orderBy: ReservationOrderByInput) {
+    reservations(orderBy: $orderBy) {
       id,
       name,
       hotelName,
@@ -16,10 +16,15 @@ const RESERVATIONS = gql`
 `
 
 const Reservations = () => {
-  const { loading, error, data } = useQuery(RESERVATIONS)
+  const { loading, error, data } = useQuery(
+    GET_RESERVATIONS,
+    { variables: { orderBy: 'arrivalDate_ASC' } },
+  )
+
   if (loading) return <Text>...loading</Text>
+
   if (error) console.log(error.message)
-  console.log(data)
+
   return (
     <View>
       {data.reservations.map((reservation: any) => (
