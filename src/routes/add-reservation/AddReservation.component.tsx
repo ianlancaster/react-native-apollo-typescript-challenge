2cbfdc5d36e24075949a7fd17a56519aa250163d
Ian Lancaster <ianclancaster@gmail.com>
2020-01-14 17:59:52 -0700

    Implement remote add reservation mutation, fix graphql/subscriptions type error.

--- src/routes/add-reservation/AddReservation.component.tsx
+++ src/routes/add-reservation/AddReservation.component.tsx
@@ -2,43 +2,55 @@ import React, { useState } from 'react'
 import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { NavigationStackScreenComponent } from 'react-navigation-stack'
 import { useGetNewReservation } from './AddReservation.queries'
-import { useUpdateNewReservation } from './AddReservation.mutations'
+import { useUpdateNewReservation, useCreateReservation } from './AddReservation.mutations'
 import styles from './AddReservation.styles'
 import { isCompleteReservation } from 'routes/reservations/Reservations.selectors'
+import { Reservation } from 'types/models'
 
-const AddReservation: NavigationStackScreenComponent = () => {
-  const { data } = useGetNewReservation()
-  if (!data) return <Text>Not Found.</Text>
-  const { screenState: { newReservation } } = data
+type Target = 'hotelName' | 'name' | 'arrivalDate' | 'departureDate'
+type LabeledInputProps = { label: string, target: Target, newReservation: Reservation }
+const LabeledInput = ({
+  label,
+  target,
+  newReservation,
+}: LabeledInputProps) => {
   const [updateNewReservation] = useUpdateNewReservation()
-  const [showError, setShowError] = useState(false)
-
-  const handleAddReservationClick = () => {
-    const dataValid = isCompleteReservation(newReservation)
-    setShowError(!dataValid)
-    if (dataValid) console.log('Adding reservation!')
-  }
-
-  type LabeledInputProps = { label: string, target: string }
-  const LabeledInput = ({ label, target }: LabeledInputProps) => (
+  return (
     <>
       <View style={styles.inputLabelContainer}>
         <Text style={styles.inputLabel}>{label}</Text>
       </View>
       <TextInput
+        key={`${target}-input`}
         style={styles.textInput}
-        value={newReservation.hotelName}
+        value={newReservation[target]}
         onChangeText={text => updateNewReservation({ variables: { target, text } })}
       />
     </>
   )
+}
+
+const AddReservation: NavigationStackScreenComponent = () => {
+  const { data } = useGetNewReservation()
+  if (!data) return <Text>Not Found.</Text>
+  const { screenState: { newReservation } } = data
+  const [createReservation] = useCreateReservation({ variables: newReservation })
+  const [showError, setShowError] = useState(false)
+
+  const handleAddReservationClick = () => {
+    const dataValid = isCompleteReservation(newReservation)
+    setShowError(!dataValid)
+    if (dataValid) createReservation()
+  }
 
   return (
     <View style={styles.screenContainer}>
-      <LabeledInput label='Hotel Name' target='hotelName' />
-      <LabeledInput label='Name' target='name' />
-      <LabeledInput label='Arrival Date' target='arrivalDate' />
-      <LabeledInput label='Departure Date' target='departureDate' />
+      <View>
+        <LabeledInput label='Hotel Name' target='hotelName' newReservation={newReservation} />
+        <LabeledInput label='Name' target='name' newReservation={newReservation} />
+        <LabeledInput label='Arrival Date' target='arrivalDate' newReservation={newReservation} />
+        <LabeledInput label='Departure Date' target='departureDate' newReservation={newReservation} />
+      </View>
 
       {showError && (
         <View style={styles.errorContainer}>
--- src/routes/add-reservation/AddReservation.mutations.ts
+++ src/routes/add-reservation/AddReservation.mutations.ts
@@ -4,6 +4,7 @@ import {
   UpdateNewReservation,
   UpdateNewReservationVariables,
 } from './graphTypes/updateNewReservation'
+import { MutationHookOptions } from '@apollo/react-hooks/lib/types'
 
 export const UPDATE_NEW_RESERVATION = gql`
   mutation UpdateNewReservation($target: String, $text: String) {
@@ -21,3 +22,27 @@ export const useUpdateNewReservation = () => useMutation<
   UpdateNewReservation,
   UpdateNewReservationVariables
 >(UPDATE_NEW_RESERVATION)
+
+export const CREATE_RESERVATION = gql`
+  mutation CreateReservation(
+    $name: String!
+    $hotelName: String!
+    $arrivalDate: String!
+    $departureDate: String!
+  ) {
+    createReservation(
+      data: {
+        name: $name,
+        hotelName: $hotelName,
+        arrivalDate: $arrivalDate,
+        departureDate: $departureDate
+      }
+    ) {
+      id
+    }
+  }
+`
+
+export const useCreateReservation = (options?: MutationHookOptions) => (
+  useMutation(CREATE_RESERVATION, options)
+)
