64cc9101f34944a9a5be5af16a8daf4c567f6a92
Ian Lancaster <ianclancaster@gmail.com>
2020-01-16 13:29:15 -0700

    Add reservations snapshot test.

--- src/routes/reservations/Reservations.component.test.tsx
+++ src/routes/reservations/Reservations.component.test.tsx
@@ -1,18 +1,27 @@
+import 'react-native'
 import React from 'react'
 import { mount } from 'enzyme'
+import renderer from 'react-test-renderer'
 import { MOCK_GET_RESERVATIONS_DATA } from 'test/fixtures'
 import { mockNavigation } from 'test/testHelpers'
 import Reservations from './Reservations.component'
 
 describe('Reservations.component', () => {
-  const wrapper = mount(
+  const ReservationsComponent = (
     <Reservations
       theme='light'
       navigation={mockNavigation}
       screenProps={{}}
       data={MOCK_GET_RESERVATIONS_DATA}
-    />,
+    />
   )
+
+  const wrapper = mount(ReservationsComponent)
+
+  it('renders correctly', () => {
+    const renderTree = renderer.create(ReservationsComponent).toJSON()
+    expect(renderTree).toMatchSnapshot()
+  })
 
   it('should render a primary button with the text "Add New Reservation" that navigates to the appropriate route', () => {
     const PrimaryButton: any = wrapper.find('PrimaryButton')
